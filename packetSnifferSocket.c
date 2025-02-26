3c0a6649c8fbcb37d9aea19cbd3b30c38cb6e763
nengkya <nengkya@yahoo.com>
2025-02-27 03:30:25 +0700

    FILE * fopen(const char * filename, const char * mode);

---
+++ packetSnifferSocket.c
@@ -0,0 +1,23 @@
+#include "stdio.h" /* FILE * */
+
+
+FILE * logFile;
+
+
+int main() {
+
+
+	// FILE * fopen(const char * filename, const char * mode);
+	logFile = fopen("log.txt", "w");
+
+	/*
+	the return value of the fopen(); is pointer to FILE structure if the file opens successfully,
+	NULL pointer if the file opening fails
+	*/
+	if (0 == logFile) printf("Unable to create file !");
+
+
+
+
+
+}
